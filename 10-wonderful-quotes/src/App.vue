802ae92564e98560a4690817d576667fef093bd9
Juan David Sánchez <juancho85@gmail.com>
2017-12-25 13:02:54 +0100

    display and add quotes

--- 10-wonderful-quotes/src/App.vue
+++ 10-wonderful-quotes/src/App.vue
@@ -1,13 +1,34 @@
 <template>
     <div class="container">
-    
+      <new-quote @quoteAdded="newQuote"></new-quote>
+      <app-quote-grid :quotes="quotes"></app-quote-grid>
     </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+  import QuoteGrid from './components/QuoteGrid.vue';
+  import NewQuote from './components/NewQuote.vue';
+
+
+  export default {
+      data: function () {
+        return {
+          maxQuotes: 10,
+          quotes: [
+            'Just a Quote to see something'
+          ]
+        }
+      },
+      components: {
+        appQuoteGrid: QuoteGrid,
+        newQuote: NewQuote
+      },
+      methods: {
+          newQuote(quote) {
+            this.quotes.push(quote);
+          }
+      }
+  }
 </script>
 
 <style>
