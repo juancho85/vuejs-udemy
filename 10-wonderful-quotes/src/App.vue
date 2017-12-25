090093108ed4fe4ac4ff92e7e9671ce37f53a03f
Juan David Sánchez <juancho85@gmail.com>
2017-12-25 20:34:34 +0100

    progress bar and alert message

--- 10-wonderful-quotes/src/App.vue
+++ 10-wonderful-quotes/src/App.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="container">
+      <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
       <new-quote @quoteAdded="newQuote"></new-quote>
       <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>
       <div class="row">
@@ -15,7 +16,7 @@
 <script>
   import QuoteGrid from './components/QuoteGrid.vue';
   import NewQuote from './components/NewQuote.vue';
-
+  import Header from './components/Header.vue';
 
   export default {
       data: function () {
@@ -28,10 +29,14 @@
       },
       components: {
         appQuoteGrid: QuoteGrid,
-        newQuote: NewQuote
+        newQuote: NewQuote,
+        appHeader: Header
       },
       methods: {
           newQuote(quote) {
+            if (this.quotes.length >= this.maxQuotes) {
+              return alert('Pelase delete quotes first!');
+            }
             this.quotes.push(quote);
           },
           deleteQuote(index) {
