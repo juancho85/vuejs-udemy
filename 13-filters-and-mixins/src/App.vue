3e185b7a3ba7c80d9a948c427830745dd90e4e0c
Juan David Sánchez <juancho85@gmail.com>
2018-01-01 23:09:02 +0100

    add filter

---
+++ 13-filters-and-mixins/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Filters and mixins</h1>
+        <p>{{ text | toUppercase }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        text: 'Hola, todos'
+      }
+    },
+    filters: {
+      toUppercase(value) {
+        return value.toUpperCase();
+      }
+    }
+
+  }
+</script>
+
+<style>
+
+</style>
