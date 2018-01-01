7c11a11370ae6f87b006a3a0ff5b9e431a5e3b0b
Juan David Sánchez <juancho85@gmail.com>
2018-01-01 22:49:44 +0100

    using multiple modifiers in a directive

--- 12-directives/src/App.vue
+++ 12-directives/src/App.vue
@@ -12,7 +12,7 @@
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>Custom directives</h1>
         <p v-highlight:background.delayed="'red'">Color this</p>
-        <p v-local-highlight:background.delayed="'red'">Color this</p>
+        <p v-local-highlight:background.delayed.blink="'red'">Color this</p>
       </div>
     </div>
   </div>
@@ -29,13 +29,34 @@
           if(binding.modifiers['delayed']) {
             delay = 3000;
           }
-          setTimeout(() => {
-            if(binding.arg == 'background') {
-              el.style.backgroundColor = binding.value;
-            } else {
-              el.style.color = binding.value;
-            }
-          }, delay);
+          if(binding.modifiers['blink']) {
+            let mainColor = binding.value;
+            let secondColor = 'blue';
+            let currentColor = mainColor;
+            setTimeout(() => {
+              setInterval(()=> {
+                currentColor == secondColor ? currentColor = mainColor : currentColor = secondColor;
+                if(binding.arg == 'background') {
+                  el.style.backgroundColor = currentColor;
+                } else {
+                  el.style.color = currentColor;
+                }
+              },1000);
+              if(binding.arg == 'background') {
+                el.style.backgroundColor = binding.value;
+              } else {
+                el.style.color = binding.value;
+              }
+            }, delay);
+          } else {
+            setTimeout(() => {
+              if(binding.arg == 'background') {
+                el.style.backgroundColor = binding.value;
+              } else {
+                el.style.color = binding.value;
+              }
+            }, delay);
+          }
         }
       }
     }
