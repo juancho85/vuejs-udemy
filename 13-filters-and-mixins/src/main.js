d69f8459fa1d4310f70b39fdfd0f3fc2171e8da0
Juan David Sánchez <juancho85@gmail.com>
2018-01-03 21:57:41 +0100

    vue fade animation with a transition

--- 13-filters-and-mixins/src/main.js
+++ 13-filters-and-mixins/src/main.js
@@ -1,17 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.filter('reverse', function(value) {
-  return value.split('').reverse().join('');
-});
-
-Vue.mixin({
-  created() {
-    console.log('Global mixin - Created hook')
-  }
-})
-
-
 new Vue({
   el: '#app',
   render: h => h(App)
