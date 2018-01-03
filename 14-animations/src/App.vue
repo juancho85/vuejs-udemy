3cdae371c1bfc12d2d5f406c0d812935f88b27c6
Juan David Sánchez <juancho85@gmail.com>
2018-01-03 22:21:15 +0100

    dynamic binding of transitions names

--- 14-animations/src/App.vue
+++ 14-animations/src/App.vue
@@ -4,16 +4,21 @@
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>Animations</h1>
         <hr>
+        <select name="alert-animation" id="alert-animation" v-model="alertAnimation" class="form-control">
+          <option value="fade">Fade</option>
+          <option value="slide">Slide</option>
+        </select>
+        <hr>
         <button class="btn btn-primary" @click="show = !show">Toggle alert</button>
         <br>
         <br>
-        <transition name="fade">
+        <transition :name="alertAnimation">
           <div class="alert alert-info" v-show="show">This is some info</div>
         </transition>
-        <transition name="slide" type="animation">
+        <transition :name="alertAnimation" type="animation">
           <div class="alert alert-info" v-show="show">This is some info</div>
         </transition>
-        <transition name="fade" appear>
+        <transition name="alertAnimation" appear>
           <div class="alert alert-info" v-if="show">This is some info</div>
         </transition>
         <transition
@@ -31,7 +36,8 @@
   export default {
     data() {
       return {
-        show: true
+        show: true,
+        alertAnimation: 'fade'
       }
     }
   }
