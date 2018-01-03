24cf2a319afed0f430df377c76c5f957e593fcbd
Juan David Sánchez <juancho85@gmail.com>
2018-01-03 22:41:35 +0100

    javascript animation hooks

--- 14-animations/src/App.vue
+++ 14-animations/src/App.vue
@@ -32,6 +32,22 @@
         >
           <div class="alert alert-info" v-if="show">This is some info</div>
         </transition>
+        <hr>
+        <button class="btn btn-primary" @click="load = !load">Load / Remove element</button>
+        <br><br>
+        <transition
+          @before-enter="beforeEnter"
+          @enter="enter"
+          @after-enter="afterEnter"
+          @enter-cancelled="enterCancelled"
+          @before-leave="beforeLeave"
+          @leave="leave"
+          @after-leave="afterLeave"
+          @leave-cancelled="leaveCancelled"
+        >
+          <div style="width: 100px; height: 100px; background-color: lightgreen" v-if="load"></div>
+        </transition>
+
 
 
       </div>
@@ -44,7 +60,36 @@
     data() {
       return {
         show: true,
-        alertAnimation: 'fade'
+        alertAnimation: 'fade',
+        load: false
+      }
+    },
+    methods: {
+      beforeEnter(el) {
+        console.log('beforeEnter');
+      },
+      enter(el, done) {
+        console.log('enter');
+        done();
+      },
+      afterEnter(el) {
+        console.log('afterEnter');
+      },
+      enterCancelled(el) {
+        console.log('enterCancelled');
+      },
+      beforeLeave(el, done) {
+        console.log('beforeLeave');
+      },
+      leave(el, done) {
+        console.log('leave');
+        done();
+      },
+      afterLeave(el) {
+        console.log('afterLeave');
+      },
+      leaveCancelled(el, done) {
+        console.log('leaveCancelled');
       }
     }
   }
