040683611627dcde64c6c4858629ef4802efa5c0
Juan David Sánchez <juancho85@gmail.com>
2018-01-07 15:40:54 +0100

    create store and define getter

--- 17vuex/src/App.vue
+++ 17vuex/src/App.vue
@@ -5,7 +5,10 @@
                 <h1>Vuex</h1>
                 <app-result></app-result>
                 <hr>
+                <app-another-result></app-another-result>
+                <hr>
                 <app-counter></app-counter>
+
             </div>
         </div>
     </div>
@@ -14,11 +17,13 @@
 <script>
     import Counter from './components/Counter.vue';
     import Result from './components/Result.vue';
+    import AnotherResult from './components/AnotherResult.vue';
 
     export default {
         components: {
             appCounter: Counter,
             appResult: Result,
+            appAnotherResult: AnotherResult,
         }
     }
 </script>
