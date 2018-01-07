0a7d9047bc9d1335fe7de31ea30a458dd90c61f3
Juan David Sánchez <juancho85@gmail.com>
2018-01-07 11:19:10 +0100

    hash fragment and scroll behavior

--- 16routing/src/components/user/UserDetail.vue
+++ 16routing/src/components/user/UserDetail.vue
@@ -4,6 +4,24 @@
       <p>User loader has ID: {{ $route.params.id }}</p>
       <router-link tag="button"
                    class="btn btn-primary"
-                   :to="{ name: 'userEdit', params: { id: $route.params.id }, query: { locale: 'en', q: 100} }">Edit User</router-link>
+                   :to="link">Edit User</router-link>
     </div>
 </template>
+
+<script>
+  export default {
+    data() {
+      return {
+        link: {
+          name: 'userEdit',
+          params: { id: this.$route.params.id },
+          query: {
+            locale: 'en',
+            q: 100
+          },
+          hash: '#data'
+        }
+      }
+    }
+  }
+</script>
--- 16routing/src/main.js
+++ 16routing/src/main.js
@@ -7,8 +7,25 @@ Vue.use(VueRouter);
 
 const router = new VueRouter({
   routes,
-  mode: 'history'
-  // ES6 transforms it in this ----> routes: routes
+  mode: 'history', // ES6 transforms it in this ----> routes: routes
+  scrollBehavior(to, from, savedPosition) {
+    // Return to previous position if any
+    if(savedPosition) {
+      return savedPosition;
+    }
+    // Otherwise, If there is a selector, go to it
+    if(to.hash) {
+      return {
+        selector: to.hash
+      }
+    }
+    // Otherwise scroll to y:700
+    return {
+      x: 0,
+      y: 700
+    }
+  }
+
 });
 
 new Vue({
