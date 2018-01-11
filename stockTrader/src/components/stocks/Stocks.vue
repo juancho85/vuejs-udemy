ec2866cfebd33b0f9700d73bf23e1de7a3e13c93
Juan David Sánchez <juancho85@gmail.com>
2018-01-11 22:22:01 +0100

    add mock data to stocks

--- stockTrader/src/components/stocks/Stocks.vue
+++ stockTrader/src/components/stocks/Stocks.vue
@@ -3,5 +3,16 @@
 </template>
 
 <script>
-  export default { }
+  export default {
+    data() {
+      return {
+        stocks: [
+          { id:1, name: 'BMW', price: 110},
+          { id:1, name: 'Google', price: 200},
+          { id:1, name: 'Apple', price: 250},
+          { id:1, name: 'Twitter', price: 8}
+        ]
+      }
+    }
+  }
 </script>
