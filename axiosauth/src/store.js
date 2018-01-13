332640f442ff7aad945b2575dad8bcb6ec164193
Juan David Sánchez <juancho85@gmail.com>
2018-01-13 23:47:34 +0100

    implement autologin (login after page update)

--- axiosauth/src/store.js
+++ axiosauth/src/store.js
@@ -43,6 +43,11 @@ export default new Vuex.Store({
             token: res.data.idToken,
             userId: res.data.localId
           })
+          const now = new Date()
+          const expirationDate = new Date(now.getTime() + res.data.expiresIn * 1000)
+          localStorage.setItem('token', res.data.idToken)
+          localStorage.setItem('userId', res.data.localId)
+          localStorage.setItem('expirationDate', expirationDate)
           dispatch('storeUser', authData)
           dispatch('setLogoutTimer', res.data.expiresIn)
         })
@@ -68,12 +73,37 @@ export default new Vuex.Store({
             token: res.data.idToken,
             userId: res.data.localId
           })
+          const now = new Date()
+          const expirationDate = new Date(now.getTime() + res.data.expiresIn * 1000)
+          localStorage.setItem('token', res.data.idToken)
+          localStorage.setItem('userId', res.data.localId)
+          localStorage.setItem('expirationDate', expirationDate)
           dispatch('setLogoutTimer', res.data.expiresIn)
         })
         .catch(error => console.log(error))
     },
+    tryAutoLogin({ commit }){
+      const token = localStorage.getItem('token')
+      if(!token) {
+        return
+      }
+      const expirationDate = localStorage.getItem('expirationDate')
+      const now = new Date()
+      if(now >= expirationDate){
+        return
+      }
+      // We still have a valid session
+      const userId = localStorage.getItem('userId')
+      commit('authUser', {
+        token,
+        userId
+      })
+    },
     logout({commit}){
       commit('clearAuthData')
+      localStorage.removeItem('token')
+      localStorage.removeItem('userId')
+      localStorage.removeItem('expirationDate')
       router.replace('/signin')
     },
     fetchUser({ commit, state }) {
